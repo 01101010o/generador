dad025bba0fefa179418064cf6f17ace8f1d3db4
01101010o <01101010o@openmailbox.org>
2015-12-25 16:53:57 -0300

    orden

--- generador.py
+++ generador.py
@@ -11,19 +11,20 @@ archivo = open(str(nombre)+'.tex', 'a')
 print('Generando encabezado')
 archivo.write('\\documentclass[11pt,a4paper]{article}\\usepackage[utf8]{inputenc}\\usepackage[T1]{fontenc}\\usepackage[spanish]{babel}\\usepackage{amsmath,amsfonts,amsthm}\\usepackage{parskip}\\usepackage[right=2cm,left=2cm,top=2cm,bottom=2cm,headsep=0cm,footskip=0.5cm]{geometry}\\title{\\textbf{Matemática}}\\author{'+str(sunombre)+'}\\date{}\\begin{document}\\maketitle\n');
 archivo.close()
-print('1)ecuaciones de segundo grado')
-print('2)ecuaciones de primer grado')
+print('0) Salir')
+print('1) Ecuaciones de primer grado')
+print('2) Ecuaciones de segundo grado')
 ejercicios = int(input('tipo de ejercicios:'))
 if ejercicios == 1:
-	r=int(input('cuantos ejercicios:'))
-	ecu2 = ECU2(r, nombre)
-	ecu2.Enunciado()
-	ecu2.Ejercicios()
-elif ejercicios == 2:
 	r=int(input('cuantos ejercicios:'))
 	ecu1 = ECU1(r, nombre)
 	ecu1.Enunciado()
 	ecu1.Ejercicios()
+elif ejercicios == 2:
+	r=int(input('cuantos ejercicios:'))
+	ecu2 = ECU2(r, nombre)
+	ecu2.Enunciado()
+	ecu2.Ejercicios()
 else:
 	print('fin')
 
